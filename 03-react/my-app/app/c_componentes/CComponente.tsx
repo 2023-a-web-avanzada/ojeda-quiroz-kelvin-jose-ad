7e877c76cbfb8f52c40558be6e6c9a984038fb20
Kelvin <kelvin.ojeda@epn.edu.ec>
2023-06-23 16:15:29 -0500

    Add: First useState

--- 03-react/my-app/app/c_componentes/CComponente.tsx
+++ 03-react/my-app/app/c_componentes/CComponente.tsx
@@ -1,26 +1,64 @@
+import {useState} from "react";
+
 export type PropiedadesComponente = {
     url: string;
     iteraciones: number;
-    mostrar?:boolean;
+    mostrar?: boolean;
 }
 
 export default function CComponente(
     props: PropiedadesComponente
-){
-    const {url, iteraciones, mostrar} = props
-    const contenidoAdicional = ()=>{
-        if(mostrar){
+) {
+    let {url, iteraciones, mostrar} = props
+    const contenidoAdicional = () => {
+        if (mostrar) {
             return <p>Mostrar</p>
         }
         return <p>Ocultar</p>
     }
-    return(
-        <>
+    const arreglo = [0, 1]
+    // const valorArregloSeleccionado = arreglo[0]
+    // const valorArregloSeleccionado02 = arreglo[1]
+    // Para ahorrarnos lineas código
+    const [valor01, valor02] = arreglo
+    const objeto = {}
+
+    const [iteracionLocal, setIteracionLocal] = useState(
+        iteraciones
+    )
+
+    const color = "bg-red-400 border-solid border"
+    const [colorLocal, setColorLocal] = useState(
+        color
+    )
+
+
+    return (
+        <div className={"border border-solid border-black m-2 p-2"}>
+
             <a href={url} target="_blank">IR A URL</a>
-            <p>Iteracion: {iteraciones}</p>
+            <p className={colorLocal}>Iteracion: {iteraciones} {iteracionLocal}</p>
             <p>Mostrar: {mostrar}</p>
             {contenidoAdicional()}
             {/*{mostrar ?<p>Mostrar con if contraido</p>:<p>Ocultar con if contraido</p>}*/}
-        </>
+            {mostrar && <p>Mostrar rápido</p>}
+            <button className={"bg-blue-400 border-solid border"} onClick={
+                (event) => {
+                    console.log(event)
+                    iteraciones += 1
+                    console.log(iteraciones)
+                    setIteracionLocal(iteracionLocal + 1)
+                    if(colorLocal === "bg-yellow-400 border-solid border") {
+                        setColorLocal("bg-red-400 border-solid border")
+                    }else {
+                        setColorLocal("bg-yellow-400 border-solid border")
+                    }
+
+                }
+            }>Imprimir evento
+            </button>
+
+        </div>
+
     )
 }
