b82ce28684156787aea681fbcd36cbd8e0bc7799
KelvinOjedaEpn <kelvin.ojeda@epn.edu.ec>
2023-06-26 06:03:41 -0500

    Add: Project 1B and modification to optimize Exam02/Exam01

--- 01-javascript/Examen02/Exam01/UserCar.js
+++ 01-javascript/Examen02/Exam01/UserCar.js
@@ -1,7 +1,11 @@
 import inquirer from 'inquirer'
-import fs from 'fs'
 import Persona from './Persona.js'
 import Carro from "./Carro.js";
+import {
+    readData,
+    saveDataPriviousData,
+    saveDataDelete
+} from './ManagerFile.js'
 
 
 async function eliminarPersona(idUsuario) {
@@ -441,68 +445,6 @@ async function solicitarDatos() {
     );
     return persona
 
-}
-
-
-function saveDataDelete(personas) {
-    try {
-        fs.writeFileSync('01-datos.json', JSON.stringify(personas))
-    } catch (error) {
-        console.error('Error al guardar los datos después de delete: ', error);
-    }
-}
-
-
-function saveDataPriviousData(persona) {
-    try {
-        let datosActuales = [];
-        if (fs.existsSync('01-datos.json')) {
-            const data = fs.readFileSync('01-datos.json', 'utf-8');
-            if (data === '') {
-                fs.writeFileSync('01-datos.json', JSON.stringify(datosActuales))
-            } else {
-                datosActuales = JSON.parse(data);
-            }
-        }
-        datosActuales.push(persona);
-        fs.writeFileSync('01-datos.json', JSON.stringify(datosActuales));
-        console.log('Datos guardados exitosamente');
-    } catch (error) {
-        console.error('Error al guardar los datos: ', error);
-    }
-}
-
-
-function readData() {
-    try {
-        const data = fs.readFileSync('01-datos.json', 'utf-8')
-        if (data === '') {
-            fs.writeFileSync('01-datos.json', JSON.stringify([]))
-        } else {
-            const parsedData = JSON.parse(data)
-            const personas = parsedData.map((item) => {
-                const persona = new Persona(
-                    item.nombre,
-                    item.edad,
-                    (item.estadoCivil === 'casado' ? true : false),
-                    item.id
-                )
-                item.carros.forEach((carro) => {
-                    const newCar = new Carro(
-                        carro.placa,
-                        carro.recorrido,
-                        carro.numeroPuertas
-                    )
-                    persona.agregarCarro(newCar)
-                })
-                return persona
-            })
-            return personas
-        }
-
-    } catch (error) {
-        console.error('Error al leer los datos: ', error.message)
-    }
 }
 
 
